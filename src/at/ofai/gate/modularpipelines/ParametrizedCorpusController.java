2f219ddee26271c50972e6f4b67cb81dde20cd49
Johann Petrak <johann.petrak@gmail.com>
2014-08-23 16:16:58 +0100

    Fixed the lock-up bug by avoiding to create a new Thread for the action.

--- src/at/ofai/gate/modularpipelines/ParametrizedCorpusController.java
+++ src/at/ofai/gate/modularpipelines/ParametrizedCorpusController.java
@@ -20,9 +20,12 @@ import gate.gui.ActionsPublisher;
 import gate.gui.MainFrame;
 import gate.gui.NewResourceDialog;
 import java.awt.event.ActionEvent;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.AbstractAction;
 import javax.swing.Action;
 import static javax.swing.Action.SHORT_DESCRIPTION;
@@ -34,12 +37,12 @@ import javax.swing.JOptionPane;
  */
 @CreoleResource(name = "Parametrized Corpus Controller",
         comment = "A conditional corpus controller that can be parametrized from a config file",
-        helpURL="https://github.com/johann-petrak/gateplugin-modularpipelines/wiki/ParametrziedCorpusController")
+        helpURL = "https://github.com/johann-petrak/gateplugin-modularpipelines/wiki/ParametrziedCorpusController")
 public class ParametrizedCorpusController extends ConditionalSerialAnalyserController
-  implements ActionsPublisher {
-  
-  private static final long serialVersionUID = 5865826533344553897L;  
-  
+        implements ActionsPublisher {
+
+  private static final long serialVersionUID = 5865826533344553897L;
+
   @Optional
   @CreoleParameter(
           comment = "The URL of the config file for setting parameters and features (.properties or .yaml)",
@@ -52,31 +55,30 @@ public class ParametrizedCorpusController extends ConditionalSerialAnalyserContr
     return configFileUrl;
   }
   protected URL configFileUrl = null;
-
   Config config;
-  
-  @Override public Resource init() {
+
+  @Override
+  public Resource init() {
     config = Utils.readConfigFile(getConfigFileUrl());
     return this;
   }
-  
+
   @Override
   public void reInit() throws ResourceInstantiationException {
     init();
-  }  
-  
+  }
+
   @Override
   public void execute() throws ExecutionException {
     Utils.setControllerParms(this, config);
     super.execute();
   }
-  
   private List<Action> actions;
-  
+
   public List<Action> getActions() {
     if (actions == null) {
       actions = new ArrayList<Action>();
-      
+
       // Action 1: re-load the config file
       actions.add(
               new AbstractAction("Re-load config") {
@@ -90,27 +92,15 @@ public class ParametrizedCorpusController extends ConditionalSerialAnalyserContr
 
         @Override
         public void actionPerformed(ActionEvent evt) {
-          Runnable runnableAction = new Runnable() {
-            @Override
-            public void run() {
-              if(getConfigFileUrl() != null) {
-                try {                
-                  MainFrame.lockGUI("Reading config file "+getConfigFileUrl()+"...");
-                  config = Utils.readConfigFile(getConfigFileUrl());
-                } finally {
-                  MainFrame.unlockGUI();
-                }
-                System.out.println("Reloaded config file "+getConfigFileUrl());
-              } else {
-                System.out.println("Nothing re-loaded, not config file set");
-              }
-            }
-          };
-          Thread thread = new Thread(runnableAction, "ModularPipelinesConfigurationReReader");
-          thread.start();
-        };
+          if (getConfigFileUrl() != null) {
+            config = Utils.readConfigFile(getConfigFileUrl());
+            System.out.println("Reloaded config file " + getConfigFileUrl());
+          } else {
+            System.out.println("Nothing re-loaded, not config file set");
+          }
+        }
       });
-      
+
       // Action 2: change the config file
       actions.add(
               new AbstractAction("Change/remove config file (no effect on init parms)") {
@@ -124,50 +114,50 @@ public class ParametrizedCorpusController extends ConditionalSerialAnalyserContr
 
         @Override
         public void actionPerformed(ActionEvent evt) {
-          Runnable runnableAction = new Runnable() {
-            @Override
-            public void run() {
-              CreoleRegister reg = Gate.getCreoleRegister();
-              String resourceClass = "at.ofai.gate.modularpipelines.ParametrizedCorpusController";
-              ResourceData rd = reg.get(resourceClass);
-              if(rd == null) {
-                JOptionPane.showMessageDialog(MainFrame.getInstance(),
-                "Error: could not find our own ParametrizedCorpusControllerClass!\n",
-                "GATE", JOptionPane.ERROR_MESSAGE);          
-                return;
-              }
-              NewResourceDialog nrd = new NewResourceDialog(MainFrame.getInstance(), "XXXXXXXXXXXXXXX", false);
-              boolean ok = nrd.show(rd,"Change or remove config file");
-              if(ok) {
-                FeatureMap parms = nrd.getSelectedParameters();
-                if(parms==null) return;
-                URL newUrl = (URL)parms.get("configFileUrl");
-                setConfigFileUrl(newUrl);
-                if (newUrl != null) {
-                  try {
-                    MainFrame.lockGUI("Reading config file " + getConfigFileUrl() + "...");
-                    config = Utils.readConfigFile(getConfigFileUrl());
-                  } finally {
-                    MainFrame.unlockGUI();
-                  }
-                  System.out.println("Reloaded config file " + getConfigFileUrl());
-                } else {
-                  config = new Config();
-                  System.out.println("Cleared config data");
-                }
-              }
+          CreoleRegister reg = Gate.getCreoleRegister();
+          String resourceClass = "at.ofai.gate.modularpipelines.ParametrizedCorpusController";
+          ResourceData rd = reg.get(resourceClass);
+          if (rd == null) {
+            JOptionPane.showMessageDialog(MainFrame.getInstance(),
+                    "Error: could not find our own ParametrizedCorpusControllerClass!\n",
+                    "GATE", JOptionPane.ERROR_MESSAGE);
+            return;
+          }
+          NewResourceDialog nrd = new NewResourceDialog(MainFrame.getInstance(), "XXXXXXXXXXXXXXX", false);
+          boolean ok = nrd.show(rd, "Change or remove config file");
+          if (ok) {
+            FeatureMap parms = nrd.getSelectedParameters();
+            if (parms == null) {
+              return;
             }
-          };
-          Thread thread = new Thread(runnableAction, "ModularPipelinesConfigurationChanger");
-          thread.start();
-        };
-      });
+            URL newUrl;
+            try {
+              String urlString = (String)parms.get("configFileUrl");
+              if(urlString == null) {
+                newUrl = null;
+              } else {
+                newUrl = new URL((String)parms.get("configFileUrl"));
+              }
+            } catch (MalformedURLException ex) {
+              ex.printStackTrace(System.err);
+              return;
+            }
+            setConfigFileUrl(newUrl);
+            if (newUrl != null) {
+              config = Utils.readConfigFile(getConfigFileUrl());
+              System.out.println("Reloaded config file " + getConfigFileUrl());
+            } else {
+              config = new Config();
+              System.out.println("Cleared config data");
+            }
+          }
+        }
+      ;
+    }
+    );
       
       
     }
     return actions;
   }
-  
-  
-  
 }
