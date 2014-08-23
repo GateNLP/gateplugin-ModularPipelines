e8086d8240b821944ecd330a4dc38c09d84f3160
Johann Petrak <johann.petrak@gmail.com>
2014-08-23 15:42:59 +0100

    Attempt to add an action that allows to change or remove the config file, but this version locks up the GUI when the dialog is shown.

--- src/at/ofai/gate/modularpipelines/ParametrizedCorpusController.java
+++ src/at/ofai/gate/modularpipelines/ParametrizedCorpusController.java
@@ -4,16 +4,21 @@
  */
 package at.ofai.gate.modularpipelines;
 
+import gate.CreoleRegister;
+import gate.FeatureMap;
+import gate.Gate;
 import gate.GateConstants;
 import gate.Resource;
 import gate.creole.ConditionalSerialAnalyserController;
 import gate.creole.ExecutionException;
+import gate.creole.ResourceData;
 import gate.creole.ResourceInstantiationException;
 import gate.creole.metadata.CreoleParameter;
 import gate.creole.metadata.CreoleResource;
 import gate.creole.metadata.Optional;
 import gate.gui.ActionsPublisher;
 import gate.gui.MainFrame;
+import gate.gui.NewResourceDialog;
 import java.awt.event.ActionEvent;
 import java.net.URL;
 import java.util.ArrayList;
@@ -21,6 +26,7 @@ import java.util.List;
 import javax.swing.AbstractAction;
 import javax.swing.Action;
 import static javax.swing.Action.SHORT_DESCRIPTION;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -70,6 +76,8 @@ public class ParametrizedCorpusController extends ConditionalSerialAnalyserContr
   public List<Action> getActions() {
     if (actions == null) {
       actions = new ArrayList<Action>();
+      
+      // Action 1: re-load the config file
       actions.add(
               new AbstractAction("Re-load config") {
         {
@@ -102,6 +110,60 @@ public class ParametrizedCorpusController extends ConditionalSerialAnalyserContr
           thread.start();
         };
       });
+      
+      // Action 2: change the config file
+      actions.add(
+              new AbstractAction("Change/remove config file (no effect on init parms)") {
+        {
+          putValue(SHORT_DESCRIPTION,
+                  "Set or change the config, or remove it completely (no effect on init parms)");
+          putValue(GateConstants.MENU_PATH_KEY,
+                  new String[]{"WTFISTHIS??????"});
+        }
+        private static final long serialVersionUID = 1L;
+
+        @Override
+        public void actionPerformed(ActionEvent evt) {
+          Runnable runnableAction = new Runnable() {
+            @Override
+            public void run() {
+              CreoleRegister reg = Gate.getCreoleRegister();
+              String resourceClass = "at.ofai.gate.modularpipelines.FakeResource";
+              ResourceData rd = reg.get(resourceClass);
+              if(rd == null) {
+                JOptionPane.showMessageDialog(MainFrame.getInstance(),
+                "Error: could not find our own ParametrizedCorpusControllerClass!\n",
+                "GATE", JOptionPane.ERROR_MESSAGE);          
+                return;
+              }
+              NewResourceDialog nrd = new NewResourceDialog(MainFrame.getInstance(), "XXXXXXXXXXXXXXX", false);
+              boolean ok = nrd.show(rd,"Change or remove config file");
+              if(ok) {
+                FeatureMap parms = nrd.getSelectedParameters();
+                if(parms==null) return;
+                URL newUrl = (URL)parms.get("configFileUrl");
+                setConfigFileUrl(newUrl);
+                if (newUrl != null) {
+                  try {
+                    MainFrame.lockGUI("Reading config file " + getConfigFileUrl() + "...");
+                    config = Utils.readConfigFile(getConfigFileUrl());
+                  } finally {
+                    MainFrame.unlockGUI();
+                  }
+                  System.out.println("Reloaded config file " + getConfigFileUrl());
+                } else {
+                  config = new Config();
+                  System.out.println("Cleared config data");
+                }
+              }
+            }
+          };
+          Thread thread = new Thread(runnableAction, "ModularPipelinesConfigurationChanger");
+          thread.start();
+        };
+      });
+      
+      
     }
     return actions;
   }
