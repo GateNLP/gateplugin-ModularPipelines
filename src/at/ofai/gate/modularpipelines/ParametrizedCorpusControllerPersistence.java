e5f6247603c045f9187674fc78205ae3095d310c
Johann Petrak <johann.petrak@gmail.com>
2016-09-29 18:59:03 +0100

    Update license and add license headers

--- src/at/ofai/gate/modularpipelines/ParametrizedCorpusControllerPersistence.java
+++ src/at/ofai/gate/modularpipelines/ParametrizedCorpusControllerPersistence.java
@@ -1,3 +1,23 @@
+/*
+ * Copyright (c) 2013 Austrian Research Institute for Artificial Intelligence (OFAI). 
+ * Copyright (C) 2014-2016 The University of Sheffield.
+ *
+ * This file is part of gateplugin-ModularPipelines
+ * (see https://github.com/johann-petrak/gateplugin-ModularPipelines)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software. If not, see <http://www.gnu.org/licenses/>.
+ */
 package at.ofai.gate.modularpipelines;
 
 import gate.FeatureMap;
@@ -28,7 +48,7 @@ public class ParametrizedCorpusControllerPersistence extends  ConditionalSerialA
     logger.debug("=== Persistence START: "+ourParms);
     // NOTE: in order to be able for a parent pipeline config file to override
     // the config settings of a sub pipeline, INCLUDING the init time settings,
-    // we would need to be able to somehow now here at this point what the
+    // we would need to be able to somehow know here at this point what the
     // parent pipeline setting is. However, I cannot see any reasonably simple
     // mechanism that would allow us to do this, so we need to stick with 
     // the general strategy of using the global system property for this.
@@ -50,7 +70,7 @@ public class ParametrizedCorpusControllerPersistence extends  ConditionalSerialA
       }
       initParamOverrides.putAll(config.prInitParms);
     }
-    // all the rest can be handled by the ConditionalSerialAbnalyserControllerPersistence,
+    // all the rest can be handled by the ConditionalSerialAnalyserControllerPersistence,
     // but we should get a ParametrizedCorpusController instance.
 
     // NOTE: This will eventually bubble up to the createObject() method for the Resource
