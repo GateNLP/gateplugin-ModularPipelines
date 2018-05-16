2b481877e8d8505e3eb8487555b4ca12579943b0
Johann Petrak <johann.petrak@gmail.com>
2018-05-16 22:45:42 +0100

    Add tests to make the build happy.

---
+++ src/test/java/at/ofai/gate/modularpipelines/tests/GappLoadingTest.java
@@ -0,0 +1,16 @@
+package at.oefai.gate.modularpipelines.tests;
+
+import gate.test.GappLoadingTestCase;
+
+/**
+ * Using this class automatically tests all pipelines for proper loading.
+ * 
+ * This class automatically tries to load all pipelines (any file with an 
+ * extension ".gapp" or ".xgapp") which reside
+ * in the main/resources/resources directory tree.
+ * 
+ * NOTE: please move/copy this class to the correct package for your plugin!
+ */
+public class GappLoadingTest extends GappLoadingTestCase {
+  
+}
---
+++ src/test/java/at/ofai/gate/modularpipelines/tests/TestingClass.java
@@ -0,0 +1,24 @@
+package at.ofai.gate.modularpipelines.tests;
+
+import gate.test.GATEPluginTests;
+import org.junit.Test;
+//import static org.junit.Assert.*;
+
+
+/**
+ * Using this class automatically prepares GATE and the plugin for testing.
+ * 
+ * This class automatically initializes GATE and loads the plugin. 
+ * Any method in this class with the "@Test" annotation will then get
+ * run with the plugin already properly loaded.
+ * 
+ * NOTE: please move/copy this class to the correct package for your plugin!
+ 
+ */
+public class TestingClass extends GATEPluginTests {
+
+  @Test
+  public void testSomething() {
+    // testing code goes here
+  }
+}
